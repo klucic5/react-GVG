7ff4dc280acd7dfc993b6b354c1cba02cd103a0f
klucic5 <klucic5@gmail.com>
2022-05-19 18:35:47 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { NavBar } from './NavBar.js';
-
+import { AiDef } from './AiDef.js';
 export default class ProfilePage extends React.Component {
   render() {
     return (
