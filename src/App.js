decda7857f90af07563ab2cdb29fbeaaa7e13c1e
klucic5 <klucic5@gmail.com>
2022-05-19 18:11:53 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default class ProfilePage extends React.Component {
 <NavBar />
         <h1>Moja prva React stranica</h1>
         <p>Zovem se XY i idem u GVG!</p>
-        <img src="https://i1.wp.com/www.srednja.hr/app/uploads/2013/05/Gimnazija-Velika-Gorica.jpg?w=1024&ssl=1" />
+        <img src="https://i1.wp.com/www.srednja.hr/app/uploads/2013/05/Gimnazija-Velika-Gorica.jpg?w=1024&ssl=1" width="200px"/>
         <NavBar />
       </div>
       
