7923033a45b94d68514400c9b8306bca72e7970a
klucic5 <klucic5@gmail.com>
2022-05-19 18:42:04 +0200

    2 files modified

--- src/App.js
+++ src/App.js
@@ -6,17 +6,16 @@ export default class ProfilePage extends React.Component {
   render() {
     return (
       <div class="parent">
-<NavBar />
+      <NavBar />
         <h1><a name="home">Umjetna inteligencija</a></h1>
-       
         <img src="https://cdn.pixabay.com/photo/2016/10/18/19/40/anatomy-1751201_960_720.png" width="200px"/>
       <div class="child">
-<AiDef />
-</div>
-<div class="child">
-<AiDef />
-</div>
-<NavBar />
+        <AiDef />
+      </div>
+      <div class="child">
+        <AiDef />
+      </div>
+      <NavBar />
       </div>
       
     );
--- src/NavBar.js
+++ src/NavBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export class NavBar extends React.Component {
   render() {
-    const pages = ['početna','AI_definicija','AI_primjena'];
+    const pages = ['početna','ai_def','ai_primjena'];
     const navLinks = pages.map(page => {
       return (
         <a style={{padding: 10+'px'}} href={'#' + page}>
